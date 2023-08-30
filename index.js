99ceb1afe4918908c6a5be403ce44ca7d919ccb3
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-08-30 17:21:17 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+function imprimirOlaMundo() {
+    console.log("ola mundo!");
+}
+imprimirOlaMundo();
+*/
+/*
+function escreverNome(nome){
+    console.log(nome);
+}
+escreverNome("enzo")
+*/
+/*
+const a = 1
+function imprimeVariavel () {
+    const b = 2 
+    console.log('variavel a', a)
+    console.log('variavel b', b)
+}
+imprimeVariavel()
+
+console.log('variavel a', a)
+console.log('variavel b', b)
+*/
+/*
+function calculaArea(altura, largura) {
+    const area = altura * largura;
+    return area;
+}
+const areaCalculada = calculaArea(5, 8);
+console.log("variavel areaCalculada: " + areaCalculada);
+console.log("chamada da função crua: " + calculaArea(5, 8));
+*/
+/*
+function numeros (numero1, numero2) {
+    const soma = numero1 + numero2;
+    return soma;
+}
+
+const somas = numeros(1, 2);
+console.log("soma : " + somas);
+*/
+/*
+function divideArrey(meuArrey){
+let b = [(meuArrey[0] /2),(meuArrey[meuArrey.length-1] /2)];
+
+return b;
+}
+let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+const resultado = divideArrey(a);
+console.log(resultado);
+*/
